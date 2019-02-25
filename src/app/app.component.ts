2b440f07b1700adeb7146cdf56a4f826e987a656
Cedric Renouleau <cedricrenouleau@macbook-pro-de-cedric.home>
2019-02-25 23:43:41 +0100

    signIn singUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyB8zygeny1W0D6ITt4n7kmbCu3rgOwo2nA',
+      authDomain: 'openclassroom-bookshelves.firebaseapp.com',
+      databaseURL: 'https://openclassroom-bookshelves.firebaseio.com',
+      projectId: 'openclassroom-bookshelves',
+      storageBucket: 'openclassroom-bookshelves.appspot.com',
+      messagingSenderId: '533612117652'
+    };
+    firebase.initializeApp(config);
+  }
 }
